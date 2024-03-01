f42610831b775cda45b97378ceeceeeafc68b151
AlexTalko <talko.alexander@gmail.com>
2024-03-01 19:53:18 +0400

    оформили main

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from classes import Category, Product
+from utils import get_products
+
+
+def main():
+    """Получаем зи файла товары по категориям"""
+    p = get_products()
+    for product in p:
+        category = Category(product['name'], product['description'], product['products'])
+        print(f"Категория: {category.name}\nОписание: {category.description}")
+        for prod in product['products']:
+            prod = Product(prod['name'], prod['description'], prod['price'],
+                           prod['quantity'])
+            print(f"Название: {prod.name}\nОписание: {prod.description}\n"
+                  f"Цена: {prod.price}")
+
+
+if __name__ == '__main__':
+    main()
