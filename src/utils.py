5261fde0fcf8c60e8497b407191dcf5d17f965ba
AlexTalko <talko.alexander@gmail.com>
2024-03-01 15:39:31 +0400

    исправили функцию загружающую список продуктов из файла

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,9 @@ import os
 
 def get_products():
     """Загружает данные товаров из products.json"""
-    with open(os.path.join('D:/pythonProject/oop_lesson_1/products.json'), 'r', encoding="utf-8") as f:
-        products = json.load(f)
-        return products
+    dir_path = os.path.dirname(os.path.dirname(__file__))
+    file_path = os.path.join(dir_path, 'products.json')
+    with open(file_path, 'r', encoding='utf-8') as f:
+        file = json.load(f)
+        return file
+
