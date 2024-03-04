6d95305d34d9a1f0df54775a034ee209aea3a8c5
AlexTalko <talko.alexander@gmail.com>
2024-03-04 10:20:39 +0400

    атрибут списка товаров - приватный добавили setter set_goods

--- src/classes.py
+++ src/classes.py
@@ -8,15 +8,19 @@ class Category:
     def __init__(self, name, description, goods):
         self.name = name
         self.description = description
-        self.goods = goods
+        self.__goods = goods
 
         Category.number_of_categories += 1
-        Category.number_of_unique_products += len(self.goods)
+        Category.number_of_unique_products += len(self.__goods)
+
+    def set_goods(self, product):
+        """Добавляет :product в список goods"""
+        self.__goods.append(product)
 
     def __repr__(self):
         return (f"{self.name}"
                 f"{self.description}"
-                f"{self.goods}"
+                f"{self.__goods}"
                 f"{self.number_of_categories}"
                 f"{self.number_of_unique_products}")
 
